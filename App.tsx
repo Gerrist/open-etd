d6354ec4f037697b73d22b40ce4dee3f2808b936
Pascal <pgerrist@icloud.com>
2022-02-15 09:57:56 +0100

    Improvements

--- App.tsx
+++ App.tsx
@@ -1,18 +1,40 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { TailwindProvider, useTailwind } from "tailwind-rn";
+import utilities from "./tailwind.json";
+import { SafeAreaView, StatusBar } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+import MainScreen from "./src/screens/Main";
+import LocationCheckScreen from "./src/screens/LocationCheck";
+import Timetable from './src/screens/Timetable';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+	return (
+		<TailwindProvider utilities={utilities}>
+			<SafeAreaView style={{ height: "100%" }}>
+				<StatusBar barStyle="light-content" />
+				<NavigationContainer>
+					<Stack.Navigator initialRouteName="Main">
+						<Stack.Screen
+							name="Main"
+							component={MainScreen}
+							options={{ headerShown: false }}
+						/>
+						<Stack.Screen
+							name="LocationCheck"
+							component={LocationCheckScreen}
+							options={{ headerShown: false }}
+						/>
+						<Stack.Screen
+							name="Timetable"
+							component={Timetable}
+							options={{ headerShown: false }}
+						/>
+					</Stack.Navigator>
+				</NavigationContainer>
+			</SafeAreaView>
+		</TailwindProvider>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
