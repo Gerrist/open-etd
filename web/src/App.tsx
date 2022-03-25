99d66b6da965d60a6dcc4827fa3e080ae37a2ed7
Pascal <pgerrist@icloud.com>
2022-03-25 13:49:28 +0100

    Main structure of app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,7 +1,32 @@
+import {useRecoilState} from "recoil";
+import {NavigationStates} from "./states/navigation";
+
 export default function App() {
-  return (
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-  )
+    const [page, setPage] = useRecoilState(NavigationStates.page);
+
+    return (
+        <>
+            {
+                page == 'home' && (
+                    <h1 className="text-3xl font-bold underline">
+                        Page Home placeholder
+                    </h1>
+                )
+            }
+            {
+                page == 'shift' && (
+                    <h1 className="text-3xl font-bold underline">
+                        Page Shift placeholder
+                    </h1>
+                )
+            }
+            {
+                page == 'trip' && (
+                    <h1 className="text-3xl font-bold underline">
+                        Page Trip placeholder
+                    </h1>
+                )
+            }
+        </>
+    )
 }
